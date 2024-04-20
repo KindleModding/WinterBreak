29f17c9a5d45702f82c956b0c5bc26c062533cc6
Bluebotlabz <69104218+Bluebotlabz@users.noreply.github.com>
2024-04-20 16:42:26 +0100

    Update hotfix build scripts

--- utils/buildHotfix.py
+++ utils/buildHotfix.py
@@ -6,3 +6,11 @@ os.remove('./newHotfix/fbink')
 
 shutil.copy('./utils/bin-armhf/gandalf', './newHotfix/gandalf')
 shutil.copy('./utils/bin-armhf/fbink', './newHotfix/fbink')
+
+# Replace branding lol
+
+branding = '"**** MOUNTSUS JB ****"' # Yes the "" is required
+with open('./newHotfix/bridge', 'r+') as file:
+    fileData = file.read()
+    file.seek(0)
+    file.write(fileData.replace('"**** SQUASH JB ****"', branding))
